59ae40a8c2b789b3e2a35f4e0b72dcfece9e679e
Aji permana sidik <ajipermanasidik37@gmail.com>
2026-01-11 17:39:27 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const toggle = document.getElementById("themeToggle");
+const body = document.body;
+
+// load saved theme
+if(localStorage.getItem("theme") === "light"){
+  body.classList.add("light");
+  toggle.textContent = "☀️";
+}
+
+toggle.addEventListener("click", () => {
+  body.classList.toggle("light");
+
+  if(body.classList.contains("light")){
+    toggle.textContent = "☀️";
+    localStorage.setItem("theme","light");
+  } else {
+    toggle.textContent = "🌙";
+    localStorage.setItem("theme","dark");
+  }
+});
